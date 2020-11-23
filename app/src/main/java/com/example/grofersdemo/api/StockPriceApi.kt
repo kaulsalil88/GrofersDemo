501f0a48eebb4b906cd9cc0a7ee03e5e5e24fefa
kaulsalil88 <kaulsalil88@gmail.com>
2020-11-23 19:39:32 +0530

    Added Logging interceptor to Retrofit

--- app/src/main/java/com/example/grofersdemo/api/StockPriceApi.kt
+++ app/src/main/java/com/example/grofersdemo/api/StockPriceApi.kt
@@ -5,36 +5,35 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.logging.Level
 
 private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
-//val interceptor =
-private const val BASE_URL = "https://www.alphavantage.co/"
-private val retrofit = Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi))
-    .addCallAdapterFactory(CoroutineCallAdapterFactory()).baseUrl(BASE_URL).build()
 
-//interface GitHubApi {
-//
-//    //Add the various actions here .
-//    @GET("repositories?q=android+language:kotlin+language:java&sort=stars&order=desc")
-//    fun getPopularAndroidRepoAsync():Deferred<List<RepositoryDataClass>>
-//}
-//
-//object GitHubApiService {
-//    val retrofitService : GitHubApi by lazy { retrofit.create(GitHubApi::class.java) }
-//}
+private const val BASE_URL = "https://www.alphavantage.co/"
+
+//addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+private val retrofit = Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory()).baseUrl(BASE_URL)
+    .client(OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)).build()).build()
+
 
 //API KEY 0D2ZREIKANHDZGFR
-interface StockApi{
+interface StockApi {
     @GET("query?function=TIME_SERIES_DAILY&apikey=0D2ZREIKANHDZGFR")
-    fun getStockDetails(@Query("symbol")stock:String):Deferred<StockApiDetails>
+    fun getStockDetails(@Query("symbol") stock: String): Deferred<StockApiDetails>
 
 }
 
-object  StockApiService {
-    val retrofitService:StockApi by lazy  { retrofit.create(StockApi::class.java)}
+
+object StockApiService {
+
+
+    val retrofitService: StockApi by lazy { retrofit.create(StockApi::class.java) }
 }
