9e2aa5d4bf80414b39f7bf608e6d121d41bdf00e
kaulsalil88 <kaulsalil88@gmail.com>
2020-11-24 14:22:40 +0530

    WIP Setting Progress Indicator and other misc things

--- app/src/main/java/com/example/grofersdemo/StockDetailsActivity.kt
+++ app/src/main/java/com/example/grofersdemo/StockDetailsActivity.kt
@@ -7,35 +7,46 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.icu.util.TimeUnit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock
 import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.util.TimeUtils
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.grofersdemo.databinding.ActivityStockDetailsBinding
 import com.example.grofersdemo.models.StockApiDetails
+import com.example.grofersdemo.viewmodels.ApiStatus
 import com.example.grofersdemo.viewmodels.StockPriceViewModel
 
 class StockDetailsActivity : AppCompatActivity() {
+
+    private val delayAfterWhichToFetchStocksMs: Long = 60 * 60 * 1000
 
     val TAG = StockDetailsActivity::class.java.name
     lateinit var stockPriceViewModel: StockPriceViewModel
     private lateinit var binding: ActivityStockDetailsBinding
 
     //Stock Symbol returned from the  Settings Activity
-    private  var stockSymbol:String = "GOOG"
+    private var stockSymbol: String = "GOOG"
 
     //Receiver to fetch stock details after an hour
     val messageReceiver = object : BroadcastReceiver() {
 
         override fun onReceive(ctx: Context?, intent: Intent?) {
+            Log.e(TAG, "BroadCast Received")
             stockPriceViewModel.getStockDetails(stockSymbol)
+
         }
 
     }
@@ -76,12 +87,24 @@ class StockDetailsActivity : AppCompatActivity() {
     //Function for observing the viewmodels live data
     fun setUpObservers() {
         stockPriceViewModel.status.observe(this, Observer {
+            when (it) {
+                ApiStatus.ERROR -> {
+                    binding.pb.visibility = View.GONE
+                }
+                ApiStatus.LOADING -> {
+                    binding.pb.visibility = View.VISIBLE
+                }
+                ApiStatus.DONE -> {
+                    binding.pb.visibility = View.GONE
+                }
+            }
 
         })
 
         stockPriceViewModel.result.observe(this, Observer {
             setStockValues(it)
-            fetchStockDataAfterAnHour()
+            //fetchStockDataAfterAnHour()
+            fetchStockUsingHandler()
 
         })
 
@@ -131,15 +154,16 @@ class StockDetailsActivity : AppCompatActivity() {
         binding.tvSymVal.text = stockApiDetails.stockMetaData?.symbol
     }
 
+    //Not Being Used
     private fun fetchStockDataAfterAnHour() {
 
         val alarmManager =
             getSystemService(Context.ALARM_SERVICE) as? AlarmManager
 
-        val broadCastIntent = Intent()
+        val broadCastIntent = Intent(this, StockDetailsActivity::class.java)
 
         broadCastIntent.action = ACTION_FETCH_STOCKS_IN_HOUR
-        broadCastIntent.setClass(this,StockDetailsActivity::class.java)
+        broadCastIntent.setClass(this, StockDetailsActivity::class.java)
 
         val pendingIntent = PendingIntent.getBroadcast(
             this,
@@ -148,13 +172,21 @@ class StockDetailsActivity : AppCompatActivity() {
             PendingIntent.FLAG_UPDATE_CURRENT
         )
         alarmManager?.set(
-            AlarmManager.ELAPSED_REALTIME_WAKEUP,
-            System.currentTimeMillis()+ 5_000,
+            AlarmManager.ELAPSED_REALTIME,
+            SystemClock.elapsedRealtime() + 2_000,
+
             pendingIntent
         )
     }
 
-    companion object{
+    private fun fetchStockUsingHandler() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            stockPriceViewModel.getStockDetails(stockSymbol)
+        }, delayAfterWhichToFetchStocksMs)
+
+    }
+
+    companion object {
         val ACTION_FETCH_STOCKS_IN_HOUR = "fetch-stock-in-hour"
         val PENDING_INTENT_RESULT_CODE = 1001
     }
